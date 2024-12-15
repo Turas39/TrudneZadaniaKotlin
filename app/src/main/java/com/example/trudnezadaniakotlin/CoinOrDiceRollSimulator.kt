c28d0add4028710347b20fcee9a218862bd47cf1
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-15 20:48:59 +0100

    Dodaj funkcję main z logiką symulacji rzutu monetą i kością oraz obsługą wyboru użytkownika

--- app/src/main/java/com/example/trudnezadaniakotlin/CoinOrDiceRollSimulator.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/CoinOrDiceRollSimulator.kt
@@ -17,3 +17,31 @@ class Dice : RollingTool {
         return (1..6).random().toString()
     }
 }
+
+fun main() {
+    println("Witaj drogi użytkowniku w symulatorze rzutu monetą i kością!")
+
+    while (true) {
+        println("Wybierz opcje:")
+        println("1. Rzut monetą")
+        println("2. Rzut kością")
+        println("3. Wyjście")
+
+        println("Twój wybór: ")
+        when (readLine()?.toIntOrNull()) {
+            1 -> {
+                val coin = Coin()
+                println("Wynik rzutu monetą: ${coin.roll()}")
+            }
+            2 -> {
+                val dice = Dice()
+                println("Wynik rzutu kostką: ${dice.roll()}")
+            }
+            3 -> {
+                println("Żegnaj!")
+                break
+            }
+            else -> println("Zły wybór, spróbuj ponownie")
+        }
+    }
+}
