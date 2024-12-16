f7e7eda17eee4feace6a9b3161f2eaf24dcfcc35
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-16 21:54:42 +0100

    Dodano klase Book z właściwościami: title, author, year ; Dodano klasę Book Manager do zarządzania książkami ; dodano metodę addBook do dodawania nowych książek

--- app/src/main/java/com/example/trudnezadaniakotlin/FavoriteBooksTracker.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/FavoriteBooksTracker.kt
@@ -1,2 +1,13 @@
 package com.example.trudnezadaniakotlin
 
+data class Book(val title: String, val author: String, val year: Int)
+
+class BookManager {
+    private val books = mutableListOf<Book>()
+
+    fun addBook(book: Book) {
+        books.add(book)
+    }
+
+
+}
