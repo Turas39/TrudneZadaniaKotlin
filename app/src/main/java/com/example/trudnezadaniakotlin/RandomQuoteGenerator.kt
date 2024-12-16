6b9b08414db9e0e7476f0462c3f4f93571c77f0a
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-16 20:07:51 +0100

    Dodanie singletonu i listy cytatów do niego

--- app/src/main/java/com/example/trudnezadaniakotlin/RandomQuoteGenerator.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/RandomQuoteGenerator.kt
@@ -1,2 +1,13 @@
 package com.example.trudnezadaniakotlin
 
+import kotlin.random.Random
+
+object Quotes {
+    private val quotesList: MutableList<String> = mutableListOf(
+        "Proponuję partyjkę gwinta.",
+        "Głupiś. Elf z cebulą dobry. Jak pomidor.",
+        "Widzę, że niektóre rzeczy się nie zmieniają. Night City to ciągle syf i kluski z paśnika."
+    )
+
+
+}
