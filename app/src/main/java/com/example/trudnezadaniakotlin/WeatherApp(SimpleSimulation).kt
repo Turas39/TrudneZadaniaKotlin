2b8d51d6a45deace2d831eaa2a9f59ead12b8e3f
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 18:28:02 +0100

    Dodano klasy Rainy i Snowy implementujące interfejs Weather. Klasy definiują metodę displayForecast i zwracają opis pogody: Rainy - Deszczowa, Snowy - Śnieżna

--- app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
@@ -9,3 +9,14 @@ class Sunny : Weather {
         return "Pogoda jest słoneczna i bezchmurna. Idealny dzień na spacer!"
     }
 }
+
+class Rainy : Weather {
+    override fun displayForecast(): String {
+        return "Na zewnątrz jest chłodno, trochę wietrznie i pada mocny deszcz. Nie zapomnij parasolki!"
+    }
+}
+class Snowy : Weather {
+    override fun displayForecast(): String {
+        return "Pada intensywny śnieg, jest zimno, a droga jest zamarznięta. Na twoim miejscu założyłbym czapkę!"
+    }
+}
