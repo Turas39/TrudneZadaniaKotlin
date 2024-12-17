9e21ba94e2f599a386bd7d9b30f4dfacdee36a2f
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 18:35:45 +0100

    Dodano klasę City z właściwościami: name, weather ; Dodano klasę main z listą miast i odpowiadającą im pogodą

--- app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
@@ -20,3 +20,15 @@ class Snowy : Weather {
         return "Pada intensywny śnieg, jest zimno, a droga jest zamarznięta. Na twoim miejscu założyłbym czapkę!"
     }
 }
+
+data class City(val name: String, val weather: Weather)
+
+fun main() {
+    val cities = listOf(
+        City("Jonkowo", Snowy()),
+        City("Olsztyn", Sunny()),
+        City("Orneta", Rainy()),
+        City("Kaplityny", Snowy()),
+        City("Łukta", Rainy())
+    )
+}
