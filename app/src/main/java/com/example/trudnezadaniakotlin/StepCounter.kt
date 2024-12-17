54de7fb4af4551aa7b0c06c1983f748720354708
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 19:25:06 +0100

    Dodanie funkcji main do testowania klasy StepCounter

--- app/src/main/java/com/example/trudnezadaniakotlin/StepCounter.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/StepCounter.kt
@@ -15,3 +15,15 @@ class StepCounter {
     }
 }
 
+fun main() {
+    val stepCounter = StepCounter()
+
+    stepCounter.makeStep()
+    stepCounter.makeStep()
+    stepCounter.makeStep()
+
+    println("Liczba kroków: ${StepCounter.stepCount}")
+
+    StepCounter.resetCounter()
+    println("Liczba kroków po resecie: ${StepCounter.stepCount}")
+}
