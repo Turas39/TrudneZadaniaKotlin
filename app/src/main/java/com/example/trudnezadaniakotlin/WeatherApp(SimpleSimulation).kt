b2ceea27e52f62e109ed427aa5c6eddd05f59338
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 18:05:43 +0100

    Dodano interfejs Weather oraz klasę Sunny która implementuje ten interfejs. Klasa Sunny definiuje metodę displayForecast zwracającą opis pogody słonecznej

--- app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/WeatherApp(SimpleSimulation).kt
@@ -1,2 +1,11 @@
 package com.example.trudnezadaniakotlin
 
+interface Weather {
+    fun displayForecast() : String
+}
+
+class Sunny : Weather {
+    override fun displayForecast(): String {
+        return "Pogoda jest słoneczna i bezchmurna. Idealny dzień na spacer!"
+    }
+}
