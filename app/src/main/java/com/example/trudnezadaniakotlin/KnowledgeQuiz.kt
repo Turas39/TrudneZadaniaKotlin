185f18fdf8ee6346eae0ea5fbea801beb42657cb
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 22:31:35 +0100

    Dodanie klasy TrueFalseQuestion implementującej interfejs Question ; Klasa zawiera tekst pytania oraz poprawną odpowiedź (boolean) ;  W klasie są metody: askQuestion która generuje tekst pytania z dwiema opcjami odpowiedzi: Prawda i Fałsz, checkAnswer która sprawdza czy podana odpowiedź jest poprawna, uwzględniając wielkość liter.

--- app/src/main/java/com/example/trudnezadaniakotlin/KnowledgeQuiz.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/KnowledgeQuiz.kt
@@ -10,6 +10,7 @@ class MultipleChoiceQuestion(
     private val options: List<String>,
     private val correctAnswer: String
 ) : Question {
+
     override fun askQuestion(): String {
         val optionsText = options.joinToString("\n") { "${options.indexOf(it) + 1}. $it" }
         return "$questionText\n$optionsText"
@@ -19,3 +20,22 @@ class MultipleChoiceQuestion(
         return answer.equals(correctAnswer, ignoreCase = true)
     }
 }
+
+class TrueFalseQuestion(
+    private val questionText: String,
+    private val correctAnswer: Boolean
+) : Question {
+
+    override fun askQuestion(): String {
+        return "$questionText\n1. Prawda\n2. Fałsz"
+    }
+
+    override fun checkAnswer(answer: String): Boolean {
+        return when(answer.lowercase()) {
+            "prawda" -> correctAnswer
+            "fałsz" -> !correctAnswer
+            else -> false
+        }
+    }
+}
+
