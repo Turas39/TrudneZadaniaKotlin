0574edb2f0b8025be0fe947860a0443cbdc1ec45
Turas <tomasz.czaplinski@cyfroweszkoly.pl>
2024-12-17 19:19:25 +0100

    Dodanie klasy StepCounter do liczenia kroków i resetowania licznika

--- app/src/main/java/com/example/trudnezadaniakotlin/StepCounter.kt
+++ app/src/main/java/com/example/trudnezadaniakotlin/StepCounter.kt
@@ -1,2 +1,17 @@
 package com.example.trudnezadaniakotlin
 
+class StepCounter {
+    companion object {
+        var stepCount: Int = 0
+            private set
+
+        fun resetCounter() {
+            stepCount = 0
+        }
+    }
+
+    fun makeStep() {
+        stepCount++
+    }
+}
+
